3b8411f30555a668a084e99b80ed526e07a368f6
Priyadharshini M <mpriyadharshinicse23@gmail.com>
2025-03-22 18:05:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let button=document.getElementById("button");
+var numberInShape=1;
+let oldvalue=0;
+button.onclick=()=>{
+    let n=document.getElementById("number").value;
+    const circle=document.getElementById("circle");
+    const square=document.getElementById("square");
+    const rectangle=document.getElementById("rectangle");
+    const box=document.getElementById("box");
+
+    n=Number(n)+Number(oldvalue);
+    for(let j=numberInShape;j<=n;j++){
+        let shape=document.createElement("div");
+        shape.innerHTML+=numberInShape;
+        if(circle.checked)
+            shape.classList.add("circle");
+        else if(square.checked)
+            shape.classList.add("square");
+        else if(rectangle.checked)
+            shape.classList.add("rectangle");
+        else
+        document.write("Invalid Input");
+        numberInShape++;
+        box.appendChild(shape);
+        oldvalue=document.getElementById("box").lastChild.innerHTML;
+
+    }
+
+}
